18951799d444f66340788f5ac1523aab32811e37
Shaurya <shauryajha35@gmail.com>
2026-06-25 22:37:33 +0530

    feat: improve instruction logic to standard solana protocols

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,11 @@ use solana_program::account_info::{AccountInfo, next_account_info};
 
 entrypoint!(process_instruction);
 
+#[derive[BorshSerialize, BorshDeserialize]]
+struct Counter{
+    count: u32
+}
+
 pub fn process_instruction(
     _program_id: &Pubkey,
     accounts: &[AccountInfo],
@@ -16,5 +21,7 @@ pub fn process_instruction(
     if !counter_acc.is_signer {
         return Err(solana_program::program_error::Missing_Required_Signatures)
     }
+
+    let counter = Counter::try_from_slice(*counter_account.data.borrow());
     Ok(())
 }
