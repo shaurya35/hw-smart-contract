f69dbeaa6661da96d7bc3960b65d5caba640bcdb
Shaurya <shauryajha35@gmail.com>
2026-06-25 21:36:43 +0530

    feat: Initial code for counter program

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,20 @@
-use solana_program::{account_info::AccountInfo, entrypoint::ProgramResult, entrypoint, pubkey::Pubkey};
+use solana_program::entrypoint::{entrypoint, ProgramResult};
+use solana_program::pubkey::{Pubkey};
+use solana_program::account_info::{AccountInfo, next_account_info};
 
 entrypoint!(process_instruction);
 
 pub fn process_instruction(
     _program_id: &Pubkey,
-    _accounts: &[AccountInfo],
+    accounts: &[AccountInfo],
     _instruction_data: &[u8],
 ) -> ProgramResult {
+
+    let mut iterate = accounts.iter();
+    let counter_acc= next_account_info(&mut iterate)?;
+
+    if !counter_acc.is_signer {
+        return Err(solana_program::program_error::Missing_Required_Signatures)
+    }
     Ok(())
 }
